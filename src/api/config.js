d402afcaedae088cfdc8e04cb94eb2f3e0d2e5ee
James Simbi <james@sparcsystems.africa>
2026-03-12 20:18:50 +0200

    use api.allorigins

--- src/api/config.js
+++ src/api/config.js
@@ -7,11 +7,12 @@ const IS_DEV = import.meta.env.DEV;
 // In prod: Using CORS proxy because of Mixed Content (HTTPS -> HTTP)
 export const AUTH_API_BASE = IS_DEV
   ? "/api/auth"
-  : "https://corsproxy.io/?http://wannie18-002-site3.btempurl.com/api";
+  : "https://api.allorigins.win/raw?url=http://wannie18-002-site3.btempurl.com/api";
 
 // Data API (Site 2 - HTTP)
 // In dev: proxy /api/data -> http://wannie18-002-site2.btempurl.com/api
 // In prod: Using CORS proxy because of Mixed Content (HTTPS -> HTTP)
 export const DATA_API_BASE = IS_DEV
   ? "/api/data"
-  : "https://corsproxy.io/?http://wannie18-002-site2.btempurl.com/api";
+  : // Alternative: Use a different CORS proxy
+    "https://api.allorigins.win/raw?url=http://wannie18-002-site2.btempurl.com/api";
